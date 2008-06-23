53b7549dbc0c102bd24178a4224a142bc96d1b51
Joerg Diekmann <joergd@pobox.com>
2008-06-23 19:16:11 +0200

    Oops - forgot two tables

--- db/migrate/20080623171151_add_indexes_for_renamed_tables.rb
+++ db/migrate/20080623171151_add_indexes_for_renamed_tables.rb
@@ -2,6 +2,8 @@ class AddIndexesForRenamedTables < ActiveRecord::Migration
   def self.up
     add_index "documents", ["account_id"], :name => "documents_account_id_index"
     add_index "documents", ["client_id"], :name => "documents_client_id_index"
+    add_index "line_items", ["document_id"], :name => "line_items_document_id_index"
+    add_index "payments", ["document_id"], :name => "payments_document_id_index"
   end
 
   def self.down
